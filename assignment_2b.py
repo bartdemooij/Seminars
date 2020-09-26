affb4fafaa3c1cff64846b767f17cc4d1a320c33
bartdemooij <71223233+bartdemooij@users.noreply.github.com>
2020-09-26 15:56:54 +0200

    Update assignment_2b.py

--- assignment_2b.py
+++ assignment_2b.py
@@ -5,7 +5,11 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2b(string_1, string_2):
-
+    """
+    The first argument string_1 will be turned into a lowercased version of the string, while string_2 will be turned into a uppercased version of the string. 
+    This function returns a dictionary of string_1 as lower case, string_2 as upper case and a combination of the two.
+    
+    """
     lower = string_1.lower()
     upper = string_2.upper()
     combined = string_1 + string_2
