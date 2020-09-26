c13b7c00830491d0f413746559b27e7b85d4aa8d
bartdemooij <71223233+bartdemooij@users.noreply.github.com>
2020-09-26 15:58:31 +0200

    Update assignment_2c.py

--- assignment_2c.py
+++ assignment_2c.py
@@ -5,7 +5,11 @@ documentation and see if he can work with it.
 No cheating! Don't show or tell hem the code directly
 """
 def function_2c(w, x, y, z):
-
+    """
+    This function returns a dictionary with multiplication of arguments x and y, division of arguments x and y, addition of arguments w and z, and subtraction of w and z.
+    
+    """
+    
     multiplication = x * y
     division = x / y
     addition = w + z
